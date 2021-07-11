e70fbe919f48634ccf5dcb0c0125ec43713b643c
DanBilic <bilic90@gmx.de>
2021-07-12 00:45:31 +0200

    navigation from third onboarding screen to dashboard added

--- app/src/main/java/com/example/dragapp/onboarding/screens/ThirdScreen.kt
+++ app/src/main/java/com/example/dragapp/onboarding/screens/ThirdScreen.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.dragapp.R
+import kotlinx.android.synthetic.main.fragment_third_screen.view.*
 
 class ThirdScreen : Fragment() {
 
@@ -14,6 +16,15 @@ class ThirdScreen : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_third_screen, container, false)
+        val view =  inflater.inflate(R.layout.fragment_third_screen, container, false)
+
+
+        view.finish_button_ts.setOnClickListener {
+
+            // navigate to dashboard
+            findNavController().navigate(R.id.action_viewPagerFragment_to_dashboardFragment)
+        }
+
+        return view
     }
 }
