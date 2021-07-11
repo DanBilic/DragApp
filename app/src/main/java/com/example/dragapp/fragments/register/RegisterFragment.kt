0b878ef40f42d2fdb7f03a252225103c36451ed9
DanBilic <bilic90@gmx.de>
2021-07-11 23:07:43 +0200

    register data binding added

--- app/src/main/java/com/example/dragapp/fragments/register/RegisterFragment.kt
+++ app/src/main/java/com/example/dragapp/fragments/register/RegisterFragment.kt
@@ -1,21 +1,66 @@
 package com.example.dragapp.fragments.register
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.dragapp.R
+import com.example.dragapp.databinding.FragmentLoginBinding
+import com.example.dragapp.databinding.FragmentRegisterBinding
+import com.example.dragapp.models.Register
+import com.example.dragapp.repositories.DragRepository
+import com.example.dragapp.viewmodels.DragViewModel
+import com.example.dragapp.viewmodels.DragViewModelFactory
 
 class RegisterFragment : Fragment() {
 
+    private lateinit var mDragViewModel: DragViewModel
+
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        // data binding
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        // set so ViewModel livedata can be observed from layout
+        //binding.lifecycleOwner = this // works only with databinding layout
+
+        // DragViewModel init
+        val dragRepository = DragRepository()
+        val dragViewModelFactory = DragViewModelFactory(dragRepository)
+        mDragViewModel = ViewModelProvider(this, dragViewModelFactory).get(DragViewModel::class.java)
+
+        binding.registerButton.setOnClickListener {
+            val name = binding.registerNameEt.text.toString()
+            val email = binding.registerEmailEt.text.toString()
+            val password = binding.registerPasswordEt.text.toString()
+
+            val registerData = Register(name, email, password)
+            mDragViewModel.register(registerData)
+            mDragViewModel.registerData.observe(viewLifecycleOwner, Observer {  response ->
+                if(response.isSuccessful){
+                    Log.d("Body:", response.body().toString())
+                    Log.d("Headers:", response.headers().toString())
+
+                }else{
+                    // Toast.makeText(requireContext(), "Invalid credentials", Toast.LENGTH_SHORT).show()
+
+                }
+            })
+        }
+
+
+        return binding.root
     }
 
 }
