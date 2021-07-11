74fb49e2b6f76acd9c9d4f0aa969700b29b7b3c6
DanBilic <bilic90@gmx.de>
2021-07-11 19:47:59 +0200

    basic navigation added

--- app/src/main/java/com/example/dragapp/fragments/login/LoginFragment.kt
+++ app/src/main/java/com/example/dragapp/fragments/login/LoginFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.dragapp.R
+import kotlinx.android.synthetic.main.fragment_login.view.*
 
 class LoginFragment : Fragment() {
 
@@ -14,7 +16,19 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val view =  inflater.inflate(R.layout.fragment_login, container, false)
+
+        view.loginButton.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_dashboardFragment)
+        }
+
+        view.registerButton.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+
+
+
+        return view;
     }
 
 }
