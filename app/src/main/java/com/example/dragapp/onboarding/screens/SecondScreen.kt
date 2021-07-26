855e48519782cc143c9bf4cb1ead836523d2c74a
DanBilic <bilic90@gmx.de>
2021-07-26 12:01:12 +0200

    ask camera and gallery permission implemented + added dexter dependency

--- app/src/main/java/com/example/dragapp/onboarding/screens/SecondScreen.kt
+++ app/src/main/java/com/example/dragapp/onboarding/screens/SecondScreen.kt
@@ -1,31 +1,116 @@
 package com.example.dragapp.onboarding.screens
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.viewpager2.widget.ViewPager2
 import com.example.dragapp.R
-import kotlinx.android.synthetic.main.fragment_first_screen.view.*
+import com.example.dragapp.databinding.FragmentLoginBinding
+import com.example.dragapp.databinding.FragmentSecondScreenBinding
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import kotlinx.android.synthetic.main.fragment_second_screen.view.*
 
 
 class SecondScreen : Fragment() {
+
+    private var _binding: FragmentSecondScreenBinding? = null
+    private val binding get() = _binding!!
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_second_screen, container, false)
+        // data binding
+        _binding = FragmentSecondScreenBinding.inflate(inflater, container, false)
+        // set so ViewModel livedata can be observed from layout
+        //binding.lifecycleOwner = this // works only with databinding layout
+
+        binding.cameraBt.setOnClickListener {
+            // Toast.makeText(requireContext(), "Camera clicked", Toast.LENGTH_SHORT).show()
+            Dexter.withContext(requireContext()).withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA
+            ).withListener(object: MultiplePermissionsListener{
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    if(report!!.areAllPermissionsGranted()){
+                        Toast.makeText(requireContext(), "Camera permission granted", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    showRasionaleDialogForPermissions()
+                }
+
+            }).onSameThread().check()
+        }
+
+        binding.galleryBt.setOnClickListener {
+            // Toast.makeText(requireContext(), "Gallery clicked", Toast.LENGTH_SHORT).show()
+            Dexter.withContext(requireContext()).withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            ).withListener(object: MultiplePermissionsListener{
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    if(report!!.areAllPermissionsGranted()){
+                        Toast.makeText(requireContext(), "Gallery permission granted", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    showRasionaleDialogForPermissions()
+                }
+
+            }).onSameThread().check()
+        }
 
         val viewPager = activity?.findViewById<ViewPager2>(R.id.viewPager)
 
-        view.next_button_ss.setOnClickListener {
+        binding.nextButtonSs.setOnClickListener {
             viewPager?.currentItem = 2
         }
 
-        return view
+        return binding.root
+    }
+
+    private fun showRasionaleDialogForPermissions() {
+        AlertDialog.Builder(requireContext()).setMessage("Truned of Permissions")
+            .setPositiveButton("Go to settings"){
+                _,_ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", requireContext().packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                }catch (e: ActivityNotFoundException){
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel"){dialog,_ -> dialog.dismiss()}
+            .show()
+
     }
 
 }
