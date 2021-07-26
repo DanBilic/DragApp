ab740458eeead4b2e25e057446e006d9858fda63
DanBilic <bilic90@gmx.de>
2021-07-26 23:43:29 +0200

    final commit

--- app/src/main/java/com/example/dragapp/fragments/discover/AddEventFragment.kt
+++ app/src/main/java/com/example/dragapp/fragments/discover/AddEventFragment.kt
@@ -1,10 +1,8 @@
 package com.example.dragapp.fragments.discover
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.dragapp.R
 
 class AddEventFragment : Fragment() {
@@ -14,7 +12,15 @@ class AddEventFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        // set menu
+        setHasOptionsMenu(true)
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_event, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_event_menu, menu)
     }
 }
